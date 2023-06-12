88672141acef826583eedb72f11d44a7bbea9e4a
ArlemSousa <fmota350@gmail.com>
2023-06-12 08:56:02 -0300

    Backpup Script Banco de dados

---
+++ PrimeiraAPI/ScriptsDB/CreateDataBase.sql
@@ -0,0 +1,23 @@
+USE [ApiTeste]
+GO
+
+/****** Object:  Table [dbo].[employee]    Script Date: 12/06/2023 08:53:08 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[employee](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](255) NULL,
+	[age] [int] NULL,
+	[photo] [varchar](255) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
